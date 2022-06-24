74b863bab2f94dc90434a8e022b13f7a6a1aab02
Ilya Makarov <feesler@gmail.com>
2022-06-24 04:29:03 +0300

    + Add Switch component

---
+++ src/demo/js/Switch.js
@@ -0,0 +1,44 @@
+import { ge, onReady } from '../../js/common.js';
+import { Switch } from '../../Components/Switch/Switch.js';
+import '../../css/common.css';
+import '../css/common.css';
+import '../css/app.css';
+import '../css/switch.css';
+
+const initParsed = () => {
+    Switch.fromElement(ge('defaultSwitch'));
+};
+
+const addEventLog = (value) => {
+    const logElem = ge('eventsLog');
+    logElem.value += `${value}\r\n`;
+};
+
+const initDynamic = () => {
+    const dynamicSwitch = Switch.create({
+        className: 'gray-switch',
+        onChange: (checked) => addEventLog(`change event. checked: ${checked}`),
+    });
+    ge('dynamicContainer').appendChild(dynamicSwitch.elem);
+
+    ge('checkBtn').addEventListener('click', () => dynamicSwitch.check(true));
+    ge('uncheckBtn').addEventListener('click', () => dynamicSwitch.check(false));
+    ge('toggleBtn').addEventListener('click', () => dynamicSwitch.toggle());
+    ge('enableBtn').addEventListener('click', () => dynamicSwitch.enable(true));
+    ge('disableBtn').addEventListener('click', () => dynamicSwitch.enable(false));
+};
+
+const initLarge = () => {
+    const largeSwitch = Switch.create({
+        className: 'large-switch',
+    });
+    ge('largeContainer').appendChild(largeSwitch.elem);
+};
+
+const init = () => {
+    initParsed();
+    initDynamic();
+    initLarge();
+};
+
+onReady(init);
