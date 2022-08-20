8753222f1251dce541801b13810da25481a95859
Ilya Makarov <feesler@gmail.com>
2022-08-20 08:39:29 +0300

    * Fix dynamic initialization of InputGroup component

--- src/demo/InputGroup/InputGroup.js
+++ src/demo/InputGroup/InputGroup.js
@@ -1,11 +1,39 @@
-import { InputGroup, onReady } from '../../js/index.js';
+import {
+    ge,
+    ce,
+    onReady,
+    InputGroup,
+} from '../../js/index.js';
 import '../../css/common.scss';
 import '../css/app.scss';
 import './style.scss';
 
-const init = () => {
+const initParsed = () => {
     const allInputs = Array.from(document.querySelectorAll('.input-group'));
     allInputs.forEach((elem) => InputGroup.fromElement(elem));
+};
+
+const initDynamic = () => {
+    const container = ge('dynContainer');
+
+    const input = ce('input', {
+        className: 'input-group__input stretch-input',
+        type: 'text',
+    });
+    const text = ce('div', {
+        className: 'input-group__text',
+        textContent: '$',
+    });
+    const inputGroup = InputGroup.create({
+        children: [input, text],
+    });
+
+    container.append(inputGroup.elem);
+};
+
+const init = () => {
+    initParsed();
+    initDynamic();
 };
 
 onReady(init);
