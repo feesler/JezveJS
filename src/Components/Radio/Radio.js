e697ce391c8dc6e9d8f01f23039a89966c95f39a
Ilya Makarov <feesler@gmail.com>
2022-10-08 22:57:30 +0300

    * Don't use state at Checkbox, Radio and Switch components to avoid issues on form reset or direct change of properties

--- src/Components/Radio/Radio.js
+++ src/Components/Radio/Radio.js
@@ -2,21 +2,17 @@ import {
     isFunction,
     setEvents,
     removeChilds,
+    enable,
     createElement,
 } from '../../js/common.js';
 import { Component } from '../../js/Component.js';
 import '../../css/common.scss';
 import './style.scss';
 
+/** CSS classes */
 const CONTAINER_CLASS = 'radio';
 const CHECK_CLASS = 'radio__check';
 const LABEL_CLASS = 'radio__label';
-const defaultProps = {
-    checked: false,
-    disabled: false,
-    label: null,
-    value: '',
-};
 
 export class Radio extends Component {
     static create(props = {}) {
@@ -31,23 +27,12 @@ export class Radio extends Component {
         return instance;
     }
 
-    constructor(props) {
-        super(props);
-
-        this.props = {
-            ...defaultProps,
-            ...this.props,
-        };
-
-        this.onChangeHandler = this.props.onChange;
-    }
-
     get checked() {
-        return this.state.checked;
+        return this.input.checked;
     }
 
     get disabled() {
-        return this.state.disabled;
+        return this.input.disabled;
     }
 
     createLabel() {
@@ -59,20 +44,7 @@ export class Radio extends Component {
             throw new Error('Name not specified');
         }
 
-        this.state = {
-            checked: this.props.checked,
-            disabled: this.props.disabled,
-            label: this.props.label,
-        };
-
-        this.input = createElement('input', {
-            props: {
-                type: 'radio',
-                name: this.props.name,
-                checked: this.state.checked,
-                value: this.props.value,
-            },
-        });
+        this.input = createElement('input', { props: { type: 'radio' } });
         this.checkIcon = createElement('span', { props: { className: CHECK_CLASS } });
         this.label = this.createLabel();
         this.elem = createElement('label', {
@@ -80,10 +52,7 @@ export class Radio extends Component {
             children: [this.input, this.checkIcon, this.label],
         });
 
-        this.setClassNames();
-        this.setHandlers();
-
-        this.render(this.state);
+        this.postInit();
     }
 
     parse(elem) {
@@ -99,109 +68,67 @@ export class Radio extends Component {
             throw new Error('Invalid structure of radio button');
         }
 
-        this.state = {
-            checked: this.input.checked,
-            disabled: elem.hasAttribute('disabled'),
-        };
+        this.postInit();
+    }
 
-        if (this.label) {
-            if (this.label.firstElementChild) {
-                this.state.label = this.label.firstElementChild;
-            } else {
-                this.state.label = this.label.textContent;
-            }
-        }
-
+    postInit() {
         this.setClassNames();
-        this.setHandlers();
-
-        this.render(this.state);
-    }
-
-    setHandlers() {
         setEvents(this.input, { change: (e) => this.onChange(e) });
-    }
 
-    onChange(e) {
-        this.setState({
-            ...this.state,
-            checked: e.target.checked,
-        });
-
-        if (isFunction(this.onChangeHandler)) {
-            this.onChangeHandler(this.state.checked);
+        // Apply props
+        if ('checked' in this.props) {
+            this.check(this.props.checked);
+        }
+        if ('disabled' in this.props) {
+            this.enable(!this.props.disabled);
+        }
+        if (typeof this.props.name === 'string') {
+            this.input.name = this.props.name;
+        }
+        if ('value' in this.props) {
+            this.input.value = this.props.value;
+        }
+        if ('label' in this.props) {
+            this.setLabel(this.props.label);
         }
     }
 
-    setState(state) {
-        if (
-            state.checked === this.state.checked
-            && state.disabled === this.state.disabled
-            && state.label === this.state.label
-        ) {
-            return;
+    onChange() {
+        if (isFunction(this.props.onChange)) {
+            this.props.onChange(this.checked);
         }
-
-        super.setState(state);
     }
 
     /** Set label content */
-    setLabel(label) {
-        this.setState({
-            ...this.state,
-            label,
-            checked: this.input.checked,
-        });
+    setLabel(value) {
+        if (!value && !this.label) {
+            return;
+        }
+
+        if (value && !this.label) {
+            this.label = this.createLabel();
+        }
+
+        if (typeof value === 'string' || value == null) {
+            this.label.textContent = value;
+        } else if (value instanceof Element) {
+            removeChilds(this.label);
+            this.label.append(value);
+        }
     }
 
     /** Set checked state */
     check(value) {
-        this.setState({
-            ...this.state,
-            checked: !!value,
-        });
+        this.input.checked = !!value;
+    }
+
+    /** Toggle checked state */
+    toggle() {
+        this.input.checked = !this.checked;
     }
 
     /** Enable/disable component */
     enable(value) {
-        this.setState({
-            ...this.state,
-            disabled: !value,
-            checked: this.input.checked,
-        });
-    }
-
-    /** Render label content */
-    renderLabel(state) {
-        if (!state.label && !this.label) {
-            return;
-        }
-
-        if (state.label && !this.label) {
-            this.label = this.createLabel();
-        }
-
-        if (typeof state.label === 'string' || state.label == null) {
-            this.label.textContent = state.label;
-        } else if (state.label instanceof Element) {
-            removeChilds(this.label);
-            this.label.appendChild(state.label);
-        }
-    }
-
-    /** Render component state */
-    render(state) {
-        if (!state) {
-            throw new Error('Invalid state');
-        }
-
-        this.input.checked = state.checked;
-        if (state.disabled) {
-            this.input.setAttribute('disabled', true);
-        } else {
-            this.input.removeAttribute('disabled');
-        }
-
-        this.renderLabel(state);
+        enable(this.input, value);
     }
 }
