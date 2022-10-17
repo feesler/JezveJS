e1fd8206400421e4b0a66a5707fb026c319f45b6
Ilya Makarov <feesler@gmail.com>
2022-10-17 07:59:21 +0300

    * Update jezve-test library and fix test scenario

--- tests/scenario/index.js
+++ tests/scenario/index.js
@@ -1,8 +1,20 @@
-import { Runner, isFullScenario } from 'jezve-test';
+import {
+    assert,
+    Runner,
+    setBlock,
+    getSelectedStory,
+} from 'jezve-test';
 import { commonTests } from './common.js';
 import { datePickerTests } from './DatePicker.js';
 import { dropDownTests } from './DropDown.js';
 import { paginatorTests } from './Paginator.js';
+
+const storiesMap = {
+    common: commonTests,
+    dropDown: dropDownTests,
+    datePicker: datePickerTests,
+    paginator: paginatorTests,
+};
 
 export class Scenario {
     constructor(environment) {
@@ -21,22 +33,36 @@ export class Scenario {
     }
 
     async run() {
-        const fullTest = isFullScenario();
-        if (fullTest) {
-            await this.runFullScenario();
+        const story = getSelectedStory();
+        if (story) {
+            setBlock(`Running '${story}' test story`, 1);
+            await this.runStory(story);
         } else {
-            await this.runTestScenario();
+            await this.runFullScenario();
         }
     }
 
-    async runTestScenario() {
-        await commonTests();
+    getStory(name) {
+        assert(name in storiesMap, `Invalid story name: ${name}`);
+
+        return storiesMap[name];
+    }
+
+    getStorieNames() {
+        return Object.keys(storiesMap);
+    }
+
+    async runStory(name) {
+        const story = this.getStory(name);
+        await story();
     }
 
     async runFullScenario() {
-        await commonTests();
-        await dropDownTests();
-        await datePickerTests();
-        await paginatorTests();
+        setBlock('Running full test scenario', 1);
+
+        const stories = this.getStorieNames();
+        for (const story of stories) {
+            await this.runStory(story);
+        }
     }
 }
