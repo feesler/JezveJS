a3fabd13eb986b54812fa2978bd687fa6f0713ed
Ilya Makarov <feesler@gmail.com>
2022-11-15 05:41:35 +0300

    + Add IconButton test component

--- packages/jezvejs-test/index.js
+++ packages/jezvejs-test/index.js
@@ -2,4 +2,5 @@ export { Checkbox } from './src/Checkbox.js';
 export { Collapsible } from './src/Collapsible.js';
 export { DatePicker } from './src/DatePicker.js';
 export { DropDown } from './src/DropDown.js';
+export { IconButton } from './src/IconButton.js';
 export { Paginator } from './src/Paginator.js';
