161d2f93a8d2868b2b7e6270f4033dca1a13e7c9
Ilya Makarov <feesler@gmail.com>
2022-11-22 03:01:50 +0300

    * Add minor load tests for all components

--- tests/view/AppView.js
+++ tests/view/AppView.js
@@ -1,21 +1,4 @@
-import { TestView, navigation, click } from 'jezve-test';
+import { TestView } from 'jezve-test';
 
 export class AppView extends TestView {
-    isUserLoggedIn() {
-        const loggedOutLocations = ['login', 'register'];
-
-        return loggedOutLocations.every((item) => !this.location.includes(`/${item}`));
-    }
-
-    async closeNotification() {
-        if (!this.content.msgPopup) {
-            return;
-        }
-
-        await this.performAction(() => this.content.msgPopup.close());
-    }
-
-    async goToMainView() {
-        await navigation(() => click(this.content.header.content.logo.linkElem));
-    }
 }
