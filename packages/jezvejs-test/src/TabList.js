aa1563cc96ffde67b2c79b30e6406875657aff24
Ilya Makarov <feesler@gmail.com>
2023-03-02 03:37:46 +0300

    * [TabList] Added test component

---
+++ packages/jezvejs-test/src/TabList.js
@@ -0,0 +1,74 @@
+import {
+    TestComponent,
+    query,
+    queryAll,
+    assert,
+    evaluate,
+    asyncMap,
+} from 'jezve-test';
+import { LinkMenu } from './LinkMenu.js';
+
+export class TabList extends TestComponent {
+    get tabs() {
+        return this.content.tabs;
+    }
+
+    get items() {
+        return this.content.items;
+    }
+
+    get selectedId() {
+        return this.content.selectedId;
+    }
+
+    get disabled() {
+        return this.content.disabled;
+    }
+
+    async parseContent() {
+        const res = {
+            tabs: await LinkMenu.create(this, await query(this.elem, '.tab-list_header')),
+            content: { elem: await query(this.elem, '.tab-list__content') },
+        };
+
+        assert(res.content.elem, 'Content element not found');
+
+        [
+            res.disabled,
+        ] = await evaluate((el) => ([
+            el.hasAttribute('disabled'),
+        ]), this.elem);
+
+        res.items = await asyncMap(
+            await queryAll(this.elem, '.tab-list__content-item'),
+            (elem) => this.parseTabContent(elem),
+        );
+
+        const activeItem = res.items.find((item) => item.active);
+        res.selectedId = activeItem?.id ?? null;
+
+        return res;
+    }
+
+    async parseTabContent(elem) {
+        assert(elem, 'Invalid element');
+
+        const res = { elem };
+
+        [
+            res.id,
+            res.active,
+            res.disabled,
+        ] = await evaluate((el) => ([
+            el.dataset.id,
+            !el.hasAttribute('hidden'),
+            el.hasAttribute('disabled'),
+        ]), elem);
+
+        return res;
+    }
+
+    async selectTabById(value) {
+        return this.tabs.selectItemByValue(value);
+    }
+}
