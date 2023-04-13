4a41592371d15d2745c7c93b58ecfe70b4d4ee24
Ilya Makarov <feesler@gmail.com>
2023-04-13 22:45:36 +0300

    * Added date utilities with locales support

--- packages/jezvejs/src/js/DateUtils.js
+++ packages/jezvejs/src/js/DateUtils.js
@@ -1,3 +1,5 @@
+import { isDate } from './common.js';
+
 export const DAYS_IN_WEEK = 7;
 export const MONTHS_COUNT = 12;
 
@@ -18,6 +20,79 @@ export const dateToLocaleDateString = (date, locales = [], options = {}) => (
 export const formatDate = (date, locales = []) => (
     dateToLocaleDateString(date, locales)
 );
+
+/** Returns object with positions of date parts and separator */
+export const getLocaleDateFormat = (locales = []) => {
+    const formatter = Intl.DateTimeFormat(locales);
+    const parts = formatter.formatToParts();
+
+    const res = {
+        dayIndex: -1,
+        monthIndex: -1,
+        yearIndex: -1,
+        separator: null,
+    };
+
+    let index = 0;
+    parts.forEach((part) => {
+        if (part.type === 'day') {
+            res.dayIndex = index;
+            index += 1;
+        }
+        if (part.type === 'month') {
+            res.monthIndex = index;
+            index += 1;
+        }
+        if (part.type === 'year') {
+            res.yearIndex = index;
+            index += 1;
+        }
+        if (part.type === 'literal') {
+            res.separator = part.value;
+        }
+    });
+
+    return res;
+};
+
+/** Returns date parsed from string accodring to specified locale */
+export const parseDateString = (str, locales = []) => {
+    if (typeof str !== 'string' || str.length === 0) {
+        return NaN;
+    }
+
+    const format = getLocaleDateFormat(locales);
+    if (
+        !format
+        || format.dayIndex === -1
+        || format.monthIndex === -1
+        || format.yearIndex === -1
+        || !format.separator
+    ) {
+        return NaN;
+    }
+
+    const dateParts = str.split(format.separator);
+    const day = parseInt(dateParts[format.dayIndex], 10);
+    const month = parseInt(dateParts[format.monthIndex], 10);
+    const year = parseInt(dateParts[format.yearIndex], 10);
+
+    if (
+        !(day >= 1 && day <= 31)
+        || !(month >= 1 && month <= 12)
+        || !(year >= 1970)
+    ) {
+        return NaN;
+    }
+
+    return new Date(Date.UTC(year, month - 1, day));
+};
+
+/** Returns true if specified argument is valid date string for current locale */
+export const isValidDateString = (str) => {
+    const date = parseDateString(str, window.app.locale);
+    return isDate(date);
+};
 
 /** Shift date to specified count of days */
 export const shiftDate = (date, shift) => (
