cbf9ee4987e376daa3f8f7e47e8db76040c06908
Ilya Makarov <feesler@gmail.com>
2023-08-05 07:42:12 +0300

    * Refactored PopupMenu demo view using DemoSection component

--- demo/src/Views/PopupMenu/PopupMenu.js
+++ demo/src/Views/PopupMenu/PopupMenu.js
@@ -1,109 +1,11 @@
 import 'jezvejs/style';
-import {
-    createElement,
-    ge,
-    setEvents,
-} from 'jezvejs';
+import { createElement, ge } from 'jezvejs';
 import { MenuButton } from 'jezvejs/MenuButton';
 import { PopupMenu } from 'jezvejs/PopupMenu';
 
 import { DemoView } from '../../Application/DemoView.js';
+import { LogsField } from '../../Components/LogsField/LogsField.js';
 import './PopupMenuView.scss';
-
-const addEventLog = (value) => {
-    const logElem = ge('eventsLog');
-    logElem.value += `${value}\r\n`;
-};
-
-const initDefault = () => {
-    const btn = MenuButton.create();
-    ge('defaultContainer').append(btn.elem);
-
-    PopupMenu.create({
-        id: 'defaultMenu',
-        attachTo: btn.elem,
-        onItemClick: (id) => addEventLog(`Item '${id}' clicked`),
-        items: [{
-            id: 'selectBtnItem',
-            icon: 'select',
-            title: 'Button item',
-            onClick: () => addEventLog('Button item clicked'),
-        }, {
-            id: 'linkItem',
-            type: 'link',
-            title: 'Link item',
-            icon: 'search',
-            url: '#',
-        }, {
-            id: 'noIconItem',
-            title: 'No icon item',
-        }, {
-            type: 'separator',
-        }, {
-            id: 'checkboxItem',
-            type: 'checkbox',
-            title: 'Checkbox item',
-            onChange: (checked) => addEventLog(`Checkbox item toggled: ${checked}`),
-        }],
-    });
-};
-
-const initAttached = () => {
-    const menu = PopupMenu.create({
-        id: 'absPosMenu',
-        hideOnScroll: false,
-        fixed: false,
-        items: [{
-            id: 'selectModeBtn',
-            icon: 'select',
-            title: 'Select',
-        }, {
-            id: 'separator1',
-            type: 'separator',
-        }, {
-            id: 'selectAllBtn',
-            title: 'Select all',
-        }],
-    });
-
-    menu.attachTo(ge('attachTarget'));
-};
-
-const initClipping = () => {
-    const btn = MenuButton.create();
-    ge('headerContent').append(btn.elem);
-
-    PopupMenu.create({
-        id: 'clippingMenu',
-        attachTo: btn.elem,
-        items: [{
-            icon: 'select',
-            title: 'Item 1',
-        }, {
-            icon: 'search',
-            title: 'Item 2',
-        }, {
-            title: 'Item 3',
-        }, {
-            type: 'separator',
-        }, {
-            title: 'Item 4',
-        }],
-    });
-};
-
-const onListMenuClick = (e, menu) => {
-    const elem = e.target.closest('.list-item');
-    if (!elem) {
-        return;
-    }
-    const btn = elem.querySelector('.menu-btn');
-    if (!btn) {
-        return;
-    }
-
-    menu.attachAndShow(btn);
-};
 
 const renderListItem = (id) => (
     createElement('div', {
@@ -118,47 +20,168 @@ const renderListItem = (id) => (
     })
 );
 
-const initList = () => {
-    const menu = PopupMenu.create({
-        id: 'listMenu',
-        items: [{
-            id: 'selectModeBtn',
-            icon: 'select',
-            title: 'Select',
-        }, {
-            id: 'separator1',
-            type: 'separator',
-        }, {
-            id: 'selectAllBtn',
-            title: 'Select all',
-        }, {
-            id: 'deselectAllBtn',
-            title: 'Clear selection',
-        }],
-    });
-
-    const list = ge('listContainer');
-    const itemsCount = 20;
-    for (let i = 1; i <= itemsCount; i += 1) {
-        const itemElem = renderListItem(i);
-        list.append(itemElem);
-    }
-    setEvents(list, { click: (e) => onListMenuClick(e, menu) });
-};
-
+/**
+ * PopupMenu component demio view
+ */
 class PopupMenuView extends DemoView {
     /**
      * View initialization
      */
     onStart() {
-        this.addContentsMenuItem({ title: 'Attach to button', url: 'attach' });
-        this.addContentsMenuItem({ title: 'Absolute position', url: 'absolute' });
-        this.addContentsMenuItem({ title: 'Attach to list item', url: 'list' });
+        this.initDefault();
+        this.initAttached();
+        this.initClipping();
+        this.initList();
+    }
 
-        initDefault();
-        initAttached();
-        initClipping();
-        initList();
+    initDefault() {
+        const logsField = LogsField.create();
+        const btn = MenuButton.create();
+
+        PopupMenu.create({
+            id: 'defaultMenu',
+            attachTo: btn.elem,
+            onItemClick: (id) => logsField.write(`Item '${id}' clicked`),
+            items: [{
+                id: 'selectBtnItem',
+                icon: 'select',
+                title: 'Button item',
+                onClick: () => logsField.write('Button item clicked'),
+            }, {
+                id: 'linkItem',
+                type: 'link',
+                title: 'Link item',
+                icon: 'search',
+                url: '#',
+            }, {
+                id: 'noIconItem',
+                title: 'No icon item',
+            }, {
+                type: 'separator',
+            }, {
+                id: 'checkboxItem',
+                type: 'checkbox',
+                title: 'Checkbox item',
+                onChange: (checked) => logsField.write(`Checkbox item toggled: ${checked}`),
+            }],
+        });
+
+        this.addSection({
+            id: 'attach',
+            title: 'Attach to button',
+            content: [
+                createElement('div', {
+                    props: { id: 'defaultContainer' },
+                    children: btn.elem,
+                }),
+                logsField.elem,
+            ],
+        });
+    }
+
+    initAttached() {
+        const menu = PopupMenu.create({
+            id: 'absPosMenu',
+            hideOnScroll: false,
+            fixed: false,
+            items: [{
+                id: 'selectModeBtn',
+                icon: 'select',
+                title: 'Select',
+            }, {
+                id: 'separator1',
+                type: 'separator',
+            }, {
+                id: 'selectAllBtn',
+                title: 'Select all',
+            }],
+        });
+
+        const attachTarget = createElement('div', {
+            props: { id: 'attachTarget', className: 'block' },
+        });
+
+        this.addSection({
+            id: 'absolute',
+            title: 'Absolute position',
+            description: '+ disabled \'hideOnScroll\' option',
+            content: attachTarget,
+        });
+
+        menu.attachTo(attachTarget);
+    }
+
+    initClipping() {
+        const btn = MenuButton.create();
+        ge('headerContent').append(btn.elem);
+
+        PopupMenu.create({
+            id: 'clippingMenu',
+            attachTo: btn.elem,
+            items: [{
+                icon: 'select',
+                title: 'Item 1',
+            }, {
+                icon: 'search',
+                title: 'Item 2',
+            }, {
+                title: 'Item 3',
+            }, {
+                type: 'separator',
+            }, {
+                title: 'Item 4',
+            }],
+        });
+    }
+
+    onListMenuClick(e, menu) {
+        const elem = e.target.closest('.list-item');
+        if (!elem) {
+            return;
+        }
+        const btn = elem.querySelector('.menu-btn');
+        if (!btn) {
+            return;
+        }
+
+        menu.attachAndShow(btn);
+    }
+
+    initList() {
+        const menu = PopupMenu.create({
+            id: 'listMenu',
+            items: [{
+                id: 'selectModeBtn',
+                icon: 'select',
+                title: 'Select',
+            }, {
+                id: 'separator1',
+                type: 'separator',
+            }, {
+                id: 'selectAllBtn',
+                title: 'Select all',
+            }, {
+                id: 'deselectAllBtn',
+                title: 'Clear selection',
+            }],
+        });
+
+        const list = createElement('div', {
+            props: { id: 'listContainer', className: 'list' },
+            events: { click: (e) => this.onListMenuClick(e, menu) },
+        });
+
+        const itemsCount = 20;
+        for (let i = 1; i <= itemsCount; i += 1) {
+            const itemElem = renderListItem(i);
+            list.append(itemElem);
+        }
+
+        this.addSection({
+            id: 'list',
+            title: 'Attach to list item',
+            content: list,
+        });
     }
 }
 
