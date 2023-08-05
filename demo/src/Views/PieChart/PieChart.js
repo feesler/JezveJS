21f47948df9a48e832884bf2b161ba099c5459b3
Ilya Makarov <feesler@gmail.com>
2023-08-05 06:05:41 +0300

    * Refactored PieChart demo view using DemoSection component

--- demo/src/Views/PieChart/PieChart.js
+++ demo/src/Views/PieChart/PieChart.js
@@ -1,24 +1,15 @@
 import 'jezvejs/style';
-import 'jezvejs/style/Button';
-import { ge, setEvents } from 'jezvejs';
+import { asArray, createElement } from 'jezvejs';
+import { Button } from 'jezvejs/Button';
 import { PieChart } from 'jezvejs/PieChart';
 
 import { DemoView } from '../../Application/DemoView.js';
 import './PieChartView.scss';
 
-const initSmall = () => {
-    const container = ge('defaultPChart');
-    const chart = PieChart.create({
-        className: 'small_pie',
-        colors: [
-            0xDCF900, 0x00B74A, 0xFF8700, 0xDB0058, 0x086CA2,
-            0xFFBA00, 0xFF3900, 0xDC0055, 0x00B64F,
-        ],
-        data: [100, 150, 120, 20, 10, 6, 8, 220],
-        radius: 50,
-    });
-    container.append(chart.elem);
-};
+const chartContainer = (id, elem) => createElement('div', {
+    props: { id, className: 'std_chart_wrap' },
+    children: asArray(elem),
+});
 
 const toggleSectorOffset = (data, sector) => (
     data.map((item) => ({
@@ -27,158 +18,212 @@ const toggleSectorOffset = (data, sector) => (
     }))
 );
 
-const initOffset = () => {
-    const hovtitle = ge('hovtitle');
-    const container = ge('offsetPChart');
-
-    let data = [
-        {
-            id: 1,
-            value: 10,
-            title: 'First category',
-            offset: 10,
-        },
-        { id: 2, value: 10, title: 'Second category' },
-        { id: 3, value: 10, title: 'Third category' },
-        { id: 4, value: 20, title: 'Fourth category' },
-    ];
-
-    const chart = PieChart.create({
-        className: 'middle_pie',
-        data,
-        radius: 100,
-        offset: 10,
-        onItemOver: ({ sector }) => {
-            hovtitle.textContent = sector.title;
-        },
-        onItemOut: () => {
-            hovtitle.textContent = '';
-        },
-        onItemClick: ({ sector }) => {
-            data = toggleSectorOffset(data, sector);
-            chart.setData(data);
-        },
-    });
-
-    container.append(chart.elem);
-};
-
-const initInnerRadius = () => {
-    const hovtitle = ge('hovtitle-inner');
-    const container = ge('innerPChart');
-
-    let data = [
-        {
-            id: 1,
-            value: 10,
-            title: 'First category',
-            offset: 10,
-        },
-        { id: 2, value: 10, title: 'Second category' },
-        { id: 3, value: 10, title: 'Third category' },
-        { id: 4, value: 20, title: 'Fourth category' },
-        { id: 5, value: 3, title: 'Fiveth category' },
-        { id: 6, value: 5, title: 'Sixth category' },
-    ];
-
-    const chart = PieChart.create({
-        className: 'middle_pie',
-        data,
-        radius: 100,
-        innerRadius: 70,
-        offset: 10,
-        onItemOver: ({ sector }) => {
-            hovtitle.textContent = sector.title;
-        },
-        onItemOut: () => {
-            hovtitle.textContent = '';
-        },
-        onItemClick: ({ sector }) => {
-            data = toggleSectorOffset(data, sector);
-            chart.setData(data);
-        },
-    });
-
-    container.append(chart.elem);
-};
-
-const initSingleValue = () => {
-    const container = ge('singleValPChart');
-    const innerContainer = ge('singleValInnerPChart');
-
-    let data = [{ value: 100 }];
-
-    const chart = PieChart.create({
-        className: 'middle_pie',
-        data,
-        radius: 100,
-        offset: 10,
-        onItemClick: ({ sector }) => {
-            data = toggleSectorOffset(data, sector);
-            chart.setData(data);
-        },
-    });
-    container.append(chart.elem);
-
-    let innerData = [{ value: 100 }];
-
-    const innerChart = PieChart.create({
-        className: 'middle_pie',
-        data: innerData,
-        radius: 100,
-        innerRadius: 70,
-        offset: 10,
-        onItemClick: ({ sector }) => {
-            innerData = toggleSectorOffset(innerData, sector);
-            innerChart.setData(innerData);
-        },
-    });
-    innerContainer.append(innerChart.elem);
-};
-
-const initNoData = () => {
-    const initialData = [
-        { category: 2, value: 10, title: 'First category' },
-        { category: 3, value: 10, title: 'Second category' },
-        { category: 5, value: 10, title: 'Third category' },
-        { category: 7, value: 20, title: 'Fourth category' },
-        { category: 8, value: 0, title: 'Fiveth category' },
-        { category: 'gray', value: 5, title: 'Sixth category' },
-    ];
-
-    const container = ge('noDataPChart');
-    const chart = PieChart.create({
-        className: 'middle_pie',
-        radius: 100,
-        innerRadius: 70,
-        offset: 10,
-        data: null,
-    });
-    container.append(chart.elem);
-
-    const setNoDataBtn = ge('setNoDataBtn');
-    setEvents(setNoDataBtn, { click: () => chart.setData(null) });
-
-    const setDataBtn = ge('setDataBtn');
-    setEvents(setDataBtn, { click: () => chart.setData(initialData) });
-};
-
+/**
+ * PieChart component demo view
+ */
 class PieChartView extends DemoView {
     /**
      * View initialization
      */
     onStart() {
-        this.addContentsMenuItem({ title: 'Default settings', url: 'default' });
-        this.addContentsMenuItem({ title: 'Title and offset sector', url: 'offset' });
-        this.addContentsMenuItem({ title: '\'innerRadius\' option', url: 'innerRadius' });
-        this.addContentsMenuItem({ title: 'Single value', url: 'single' });
-        this.addContentsMenuItem({ title: 'Set data', url: 'setData' });
+        this.initSmall();
+        this.initOffset();
+        this.initInnerRadius();
+        this.initSingleValue();
+        this.initNoData();
+    }
 
-        initSmall();
-        initOffset();
-        initInnerRadius();
-        initSingleValue();
-        initNoData();
+    initSmall() {
+        const chart = PieChart.create({
+            className: 'small_pie',
+            colors: [
+                0xDCF900, 0x00B74A, 0xFF8700, 0xDB0058, 0x086CA2,
+                0xFFBA00, 0xFF3900, 0xDC0055, 0x00B64F,
+            ],
+            data: [100, 150, 120, 20, 10, 6, 8, 220],
+            radius: 50,
+        });
+
+        this.addSection({
+            id: 'default',
+            title: 'Default settings',
+            content: chartContainer('defaultPChart', chart.elem),
+        });
+    }
+
+    initOffset() {
+        const hovtitle = createElement('div', {
+            props: { id: 'hovtitle', className: 'info-bottom' },
+        });
+
+        let data = [
+            {
+                id: 1,
+                value: 10,
+                title: 'First category',
+                offset: 10,
+            },
+            { id: 2, value: 10, title: 'Second category' },
+            { id: 3, value: 10, title: 'Third category' },
+            { id: 4, value: 20, title: 'Fourth category' },
+        ];
+
+        const chart = PieChart.create({
+            className: 'middle_pie',
+            data,
+            radius: 100,
+            offset: 10,
+            onItemOver: ({ sector }) => {
+                hovtitle.textContent = sector.title;
+            },
+            onItemOut: () => {
+                hovtitle.textContent = '';
+            },
+            onItemClick: ({ sector }) => {
+                data = toggleSectorOffset(data, sector);
+                chart.setData(data);
+            },
+        });
+
+        this.addSection({
+            id: 'offset',
+            title: 'Title and offset sector',
+            content: [
+                chartContainer('offsetPChart', chart.elem),
+                hovtitle,
+            ],
+        });
+    }
+
+    initInnerRadius() {
+        const hovtitle = createElement('div', {
+            props: { id: 'hovtitle-inner', className: 'info-inner' },
+        });
+
+        let data = [
+            {
+                id: 1,
+                value: 10,
+                title: 'First category',
+                offset: 10,
+            },
+            { id: 2, value: 10, title: 'Second category' },
+            { id: 3, value: 10, title: 'Third category' },
+            { id: 4, value: 20, title: 'Fourth category' },
+            { id: 5, value: 3, title: 'Fiveth category' },
+            { id: 6, value: 5, title: 'Sixth category' },
+        ];
+
+        const chart = PieChart.create({
+            className: 'middle_pie',
+            data,
+            radius: 100,
+            innerRadius: 70,
+            offset: 10,
+            onItemOver: ({ sector }) => {
+                hovtitle.textContent = sector.title;
+            },
+            onItemOut: () => {
+                hovtitle.textContent = '';
+            },
+            onItemClick: ({ sector }) => {
+                data = toggleSectorOffset(data, sector);
+                chart.setData(data);
+            },
+        });
+
+        this.addSection({
+            id: 'innerRadius',
+            title: '\'innerRadius\' option',
+            content: chartContainer('innerPChart', [
+                chart.elem,
+                hovtitle,
+            ]),
+        });
+    }
+
+    initSingleValue() {
+        let data = [{ value: 100 }];
+
+        const chart = PieChart.create({
+            className: 'middle_pie',
+            data,
+            radius: 100,
+            offset: 10,
+            onItemClick: ({ sector }) => {
+                data = toggleSectorOffset(data, sector);
+                chart.setData(data);
+            },
+        });
+
+        let innerData = [{ value: 100 }];
+
+        const innerChart = PieChart.create({
+            className: 'middle_pie',
+            data: innerData,
+            radius: 100,
+            innerRadius: 70,
+            offset: 10,
+            onItemClick: ({ sector }) => {
+                innerData = toggleSectorOffset(innerData, sector);
+                innerChart.setData(innerData);
+            },
+        });
+
+        this.addSection({
+            id: 'single',
+            title: 'Single value',
+            content: [
+                chartContainer('singleValPChart', chart.elem),
+                chartContainer('singleValInnerPChart', innerChart.elem),
+            ],
+        });
+    }
+
+    initNoData() {
+        const initialData = [
+            { category: 2, value: 10, title: 'First category' },
+            { category: 3, value: 10, title: 'Second category' },
+            { category: 5, value: 10, title: 'Third category' },
+            { category: 7, value: 20, title: 'Fourth category' },
+            { category: 8, value: 0, title: 'Fiveth category' },
+            { category: 'gray', value: 5, title: 'Sixth category' },
+        ];
+
+        const chart = PieChart.create({
+            className: 'middle_pie',
+            radius: 100,
+            innerRadius: 70,
+            offset: 10,
+            data: null,
+        });
+
+        const controls = createElement('div', {
+            props: { className: 'section-controls' },
+            children: [
+                Button.create({
+                    id: 'setNoDataBtn',
+                    title: 'Set no data',
+                    className: 'action-btn',
+                    onClick: () => chart.setData(null),
+                }).elem,
+                Button.create({
+                    id: 'setDataBtn',
+                    title: 'Set data',
+                    className: 'action-btn',
+                    onClick: () => chart.setData(initialData),
+                }).elem,
+            ],
+        });
+
+        this.addSection({
+            id: 'setData',
+            title: 'Set data',
+            content: [
+                chartContainer('noDataPChart', chart.elem),
+                controls,
+            ],
+        });
     }
 }
 
