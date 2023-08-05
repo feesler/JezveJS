f6a84a20a7038e0679e15d5ed21f679dac781b1c
Ilya Makarov <feesler@gmail.com>
2023-08-05 05:13:31 +0300

    * Refactored Offcanvas demo view using DemoSection component

--- demo/src/Views/Offcanvas/Offcanvas.js
+++ demo/src/Views/Offcanvas/Offcanvas.js
@@ -1,94 +1,160 @@
 import 'jezvejs/style';
-import 'jezvejs/style/Button';
-import { ge, setEvents } from 'jezvejs';
+import { createElement, ge, setEvents } from 'jezvejs';
+import { Button } from 'jezvejs/Button';
 import { Offcanvas } from 'jezvejs/Offcanvas';
 
 import { DemoView } from '../../Application/DemoView.js';
+import { LogsField } from '../../Components/LogsField/LogsField.js';
 import './OffcanvasView.scss';
 
-const logTo = (target, value) => {
-    const elem = (typeof target === 'string') ? ge(target) : target;
-    if (!elem) {
-        return;
-    }
-
-    elem.value += `${value}\r\n`;
-};
-
-const initDefault = () => {
-    const offcanvas = Offcanvas.create({
-        content: ge('defaultContent'),
-        onOpened: () => logTo('result', 'Opened'),
-        onClosed: () => logTo('result', 'Closed'),
-    });
-
-    setEvents(ge('showDefaultBtn'), { click: () => offcanvas.open() });
-    setEvents(ge('toggleTopBtn'), { click: () => offcanvas.toggle() });
-};
-
-const initRight = () => {
-    const offcanvas = Offcanvas.create({
-        content: ge('rightContent'),
-        placement: 'right',
-    });
-
-    setEvents(ge('showRightBtn'), { click: () => offcanvas.open() });
-};
-
-const initTop = () => {
-    const offcanvas = Offcanvas.create({
-        content: ge('topContent'),
-        placement: 'top',
-    });
-
-    setEvents(ge('showTopBtn'), { click: () => offcanvas.open() });
-};
-
-const initBottom = () => {
-    const offcanvas = Offcanvas.create({
-        content: ge('bottomContent'),
-        placement: 'bottom',
-    });
-
-    setEvents(ge('showBottomBtn'), { click: () => offcanvas.open() });
-};
-
-const initResponsive = () => {
-    const offcanvas = Offcanvas.create({
-        content: ge('responsiveContent'),
-        className: 'offcanvas-responsive',
-    });
-
-    setEvents(ge('showResponsiveBtn'), { click: () => offcanvas.open() });
-};
-
-const initUseScrollLock = () => {
-    const offcanvas = Offcanvas.create({
-        useScrollLock: false,
-        content: ge('useScrollLockContent'),
-    });
-
-    setEvents(ge('showScrollLockBtn'), { click: () => offcanvas.open() });
-};
-
+/**
+ * Offcanvas component demo view
+ */
 class OffcanvasView extends DemoView {
     /**
      * View initialization
      */
     onStart() {
-        this.addContentsMenuItem({ title: 'Default settings', url: 'default' });
-        this.addContentsMenuItem({ title: 'Right placement', url: 'right' });
-        this.addContentsMenuItem({ title: 'Top placement', url: 'top' });
-        this.addContentsMenuItem({ title: 'Bottom placement', url: 'bottom' });
-        this.addContentsMenuItem({ title: 'Responsive', url: 'responsive' });
-        this.addContentsMenuItem({ title: '\'useScrollLock\' option', url: 'useScrollLock' });
+        this.initDefault();
+        this.initRight();
+        this.initTop();
+        this.initBottom();
+        this.initResponsive();
+        this.initUseScrollLock();
+    }
 
-        initDefault();
-        initRight();
-        initTop();
-        initBottom();
-        initResponsive();
-        initUseScrollLock();
+    initDefault() {
+        const logsField = LogsField.create();
+
+        const offcanvas = Offcanvas.create({
+            content: ge('defaultContent'),
+            onOpened: () => logsField.write('Opened'),
+            onClosed: () => logsField.write('Closed'),
+        });
+
+        setEvents(ge('toggleTopBtn'), { click: () => offcanvas.toggle() });
+
+        this.addSection({
+            id: 'default',
+            title: 'Default settings',
+            content: [
+                createElement('div', {
+                    props: { className: 'section-controls' },
+                    children: Button.create({
+                        id: 'showDefaultBtn',
+                        className: 'action-btn',
+                        title: 'Show',
+                        onClick: () => offcanvas.open(),
+                    }).elem,
+                }),
+                logsField.elem,
+            ],
+        });
+    }
+
+    initRight() {
+        const offcanvas = Offcanvas.create({
+            content: ge('rightContent'),
+            placement: 'right',
+        });
+
+        this.addSection({
+            id: 'right',
+            title: 'Right placement',
+            content: createElement('div', {
+                props: { className: 'section-controls' },
+                children: Button.create({
+                    id: 'showRightBtn',
+                    className: 'action-btn',
+                    title: 'Show',
+                    onClick: () => offcanvas.open(),
+                }).elem,
+            }),
+        });
+    }
+
+    initTop() {
+        const offcanvas = Offcanvas.create({
+            content: ge('topContent'),
+            placement: 'top',
+        });
+
+        this.addSection({
+            id: 'top',
+            title: 'Top placement',
+            content: createElement('div', {
+                props: { className: 'section-controls' },
+                children: Button.create({
+                    id: 'showTopBtn',
+                    className: 'action-btn',
+                    title: 'Show',
+                    onClick: () => offcanvas.open(),
+                }).elem,
+            }),
+        });
+    }
+
+    initBottom() {
+        const offcanvas = Offcanvas.create({
+            content: ge('bottomContent'),
+            placement: 'bottom',
+        });
+
+        this.addSection({
+            id: 'bottom',
+            title: 'Bottom placement',
+            content: createElement('div', {
+                props: { className: 'section-controls' },
+                children: Button.create({
+                    id: 'showBottomBtn',
+                    className: 'action-btn',
+                    title: 'Show',
+                    onClick: () => offcanvas.open(),
+                }).elem,
+            }),
+        });
+    }
+
+    initResponsive() {
+        const offcanvas = Offcanvas.create({
+            content: ge('responsiveContent'),
+            className: 'offcanvas-responsive',
+        });
+
+        this.addSection({
+            id: 'responsive',
+            title: 'Responsive',
+            content: createElement('div', {
+                props: { className: 'section-controls' },
+                children: Button.create({
+                    id: 'showResponsiveBtn',
+                    className: 'action-btn',
+                    title: 'Show',
+                    onClick: () => offcanvas.open(),
+                }).elem,
+            }),
+        });
+    }
+
+    initUseScrollLock() {
+        const offcanvas = Offcanvas.create({
+            useScrollLock: false,
+            content: ge('useScrollLockContent'),
+        });
+
+        this.addSection({
+            id: 'useScrollLock',
+            title: '\'useScrollLock\' option',
+            content: createElement('div', {
+                props: { className: 'section-controls' },
+                children: Button.create({
+                    id: 'showScrollLockBtn',
+                    className: 'action-btn',
+                    title: 'Show',
+                    onClick: () => offcanvas.open(),
+                }).elem,
+            }),
+        });
     }
 }
 
