ef562084a2d6ea4ff5de7c647ce989de990a2c49
Ilya Makarov <feesler@gmail.com>
2023-08-06 03:19:21 +0300

    * Refactored Slider demo view using DemoSection component

--- demo/src/Views/Slider/Slider.js
+++ demo/src/Views/Slider/Slider.js
@@ -1,143 +1,184 @@
 import 'jezvejs/style';
-import 'jezvejs/style/Button';
-import {
-    createElement,
-    ge,
-    setEvents,
-} from 'jezvejs';
+import { createElement } from 'jezvejs';
+import { Button } from 'jezvejs/Button';
 import { Slider } from 'jezvejs/Slider';
 
 import { DemoView } from '../../Application/DemoView.js';
 import './SliderView.scss';
 
+const createContainer = (id, children) => createElement('div', {
+    props: { id },
+    children,
+});
+
 const createSlideContent = (text) => createElement('div', {
     props: { className: 'slide-content', textContent: text },
 });
 
-const initDefaultSlider = () => {
-    const slider = new Slider({
-        width: 300,
-        height: 200,
-        items: [{
-            id: 'horslide',
-            name: 'horslide-1',
-            content: createSlideContent('Slide 1'),
-        }, {
-            name: 'horslide-2',
-            content: createSlideContent('Slide 2'),
-        }, {
-            name: 'horslide-3',
-            content: createSlideContent('Slide 3'),
-        }],
-    });
-    const container = ge('defaultSlider');
-    container.append(slider.elem);
+const createSliderControls = (slider) => (
+    createElement('div', {
+        props: { className: 'section-controls' },
+        children: [
+            Button.create({
+                className: 'action-btn slide-to-start-btn',
+                title: 'Slide to start',
+                onClick: () => slider.slideTo(0),
+            }).elem,
+            Button.create({
+                className: 'action-btn slide-to-prev-btn',
+                title: 'Prev',
+                onClick: () => slider.slideToPrev(),
+            }).elem,
+            Button.create({
+                className: 'action-btn slide-to-next-btn',
+                title: 'Next',
+                onClick: () => slider.slideToNext(),
+            }).elem,
+            Button.create({
+                className: 'action-btn switch-to-start-btn',
+                title: 'Switch to start',
+                onClick: () => slider.switchTo(0),
+            }).elem,
+        ],
+    })
+);
 
-    setEvents(ge('slideToStartBtn'), { click: () => slider.slideTo(0) });
-    setEvents(ge('slidePrevBtn'), { click: () => slider.slideToPrev() });
-    setEvents(ge('slideNextBtn'), { click: () => slider.slideToNext() });
-    setEvents(ge('switchToStartBtn'), { click: () => slider.switchTo(0) });
-};
-
-const initVerticalSlider = () => {
-    const slider = new Slider({
-        width: 300,
-        height: 200,
-        vertical: true,
-    });
-    const container = ge('verticalSlider');
-    container.append(slider.elem);
-
-    slider.addSlide({
-        id: 'vslide',
-        name: 'vertslide-1',
-        content: createSlideContent('Slide 1'),
-    });
-    slider.addSlide({
-        name: 'vertslide-2',
-        content: createSlideContent('Slide 2'),
-    });
-    slider.addSlide({
-        name: 'vertslide-3',
-        content: createSlideContent('Slide 3'),
-    });
-
-    setEvents(ge('vSlideToStartBtn'), { click: () => slider.slideTo(0) });
-    setEvents(ge('vSlidePrevBtn'), { click: () => slider.slideToPrev() });
-    setEvents(ge('vSlideNextBtn'), { click: () => slider.slideToNext() });
-    setEvents(ge('vSwitchToStartBtn'), { click: () => slider.switchTo(0) });
-};
-
-const initMouseSlider = () => {
-    const slider = new Slider({
-        width: 300,
-        height: 200,
-        allowMouse: true,
-        items: [{
-            content: createSlideContent('Slide 1'),
-        }, {
-            content: createSlideContent('Slide 2'),
-        }, {
-            content: createSlideContent('Slide 3'),
-        }],
-    });
-    const container = ge('mouseSlider');
-    container.append(slider.elem);
-};
-
-const initNoTouchSlider = () => {
-    const slider = new Slider({
-        width: 300,
-        height: 200,
-        allowTouch: false,
-        items: [{
-            content: createSlideContent('Slide 1'),
-        }, {
-            content: createSlideContent('Slide 2'),
-        }, {
-            content: createSlideContent('Slide 3'),
-        }],
-    });
-    const container = ge('noTouchSlider');
-    container.append(slider.elem);
-
-    setEvents(ge('noTouchPrevBtn'), { click: () => slider.slideToPrev() });
-    setEvents(ge('noTouchNextBtn'), { click: () => slider.slideToNext() });
-};
-
-const initNoWheelSlider = () => {
-    const slider = new Slider({
-        width: 300,
-        height: 200,
-        allowWheel: false,
-        items: [{
-            content: createSlideContent('Slide 1'),
-        }, {
-            content: createSlideContent('Slide 2'),
-        }, {
-            content: createSlideContent('Slide 3'),
-        }],
-    });
-    const container = ge('noWheelSlider');
-    container.append(slider.elem);
-};
-
+/**
+ * Slider component demo view
+ */
 class SliderView extends DemoView {
     /**
      * View initialization
      */
     onStart() {
-        this.addContentsMenuItem({ title: 'Default settings', url: 'default' });
-        this.addContentsMenuItem({ title: 'Vertical', url: 'placeholder' });
-        this.addContentsMenuItem({ title: 'Enabled \'allowMouse\' option', url: 'allowMouse' });
-        this.addContentsMenuItem({ title: 'Disabled \'allowTouch\' option', url: 'allowTouch' });
-        this.addContentsMenuItem({ title: 'Disabled \'allowWheel\' option', url: 'allowWheel' });
+        this.initDefaultSlider();
+        this.initVerticalSlider();
+        this.initMouseSlider();
+        this.initNoTouchSlider();
+        this.initNoWheelSlider();
+    }
 
-        initDefaultSlider();
-        initVerticalSlider();
-        initMouseSlider();
-        initNoTouchSlider();
-        initNoWheelSlider();
+    initDefaultSlider() {
+        const slider = new Slider({
+            width: 300,
+            height: 200,
+            items: [{
+                id: 'horslide',
+                name: 'horslide-1',
+                content: createSlideContent('Slide 1'),
+            }, {
+                name: 'horslide-2',
+                content: createSlideContent('Slide 2'),
+            }, {
+                name: 'horslide-3',
+                content: createSlideContent('Slide 3'),
+            }],
+        });
+
+        this.addSection({
+            id: 'default',
+            title: 'Default settings',
+            content: [
+                createContainer('defaultSlider', slider.elem),
+                createSliderControls(slider),
+            ],
+        });
+    }
+
+    initVerticalSlider() {
+        const slider = new Slider({
+            width: 300,
+            height: 200,
+            vertical: true,
+        });
+        slider.addSlide({
+            id: 'vslide',
+            name: 'vertslide-1',
+            content: createSlideContent('Slide 1'),
+        });
+        slider.addSlide({
+            name: 'vertslide-2',
+            content: createSlideContent('Slide 2'),
+        });
+        slider.addSlide({
+            name: 'vertslide-3',
+            content: createSlideContent('Slide 3'),
+        });
+
+        this.addSection({
+            id: 'placeholder',
+            title: 'Vertical',
+            content: [
+                createContainer('verticalSlider', slider.elem),
+                createSliderControls(slider),
+            ],
+        });
+    }
+
+    initMouseSlider() {
+        const slider = new Slider({
+            width: 300,
+            height: 200,
+            allowMouse: true,
+            items: [{
+                content: createSlideContent('Slide 1'),
+            }, {
+                content: createSlideContent('Slide 2'),
+            }, {
+                content: createSlideContent('Slide 3'),
+            }],
+        });
+
+        this.addSection({
+            id: 'allowMouse',
+            title: 'Enabled \'allowMouse\' option',
+            content: createContainer('mouseSlider', slider.elem),
+        });
+    }
+
+    initNoTouchSlider() {
+        const slider = new Slider({
+            width: 300,
+            height: 200,
+            allowTouch: false,
+            items: [{
+                content: createSlideContent('Slide 1'),
+            }, {
+                content: createSlideContent('Slide 2'),
+            }, {
+                content: createSlideContent('Slide 3'),
+            }],
+        });
+
+        this.addSection({
+            id: 'allowTouch',
+            title: 'Disabled \'allowTouch\' option',
+            content: [
+                createContainer('noTouchSlider', slider.elem),
+                createSliderControls(slider),
+            ],
+        });
+    }
+
+    initNoWheelSlider() {
+        const slider = new Slider({
+            width: 300,
+            height: 200,
+            allowWheel: false,
+            items: [{
+                content: createSlideContent('Slide 1'),
+            }, {
+                content: createSlideContent('Slide 2'),
+            }, {
+                content: createSlideContent('Slide 3'),
+            }],
+        });
+
+        this.addSection({
+            id: 'allowWheel',
+            title: 'Disabled \'allowWheel\' option',
+            content: createContainer('noWheelSlider', slider.elem),
+        });
     }
 }
 
