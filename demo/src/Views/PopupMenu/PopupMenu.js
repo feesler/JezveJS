223a82c738855838e43270f0992d0a945ea9f667
Ilya Makarov <feesler@gmail.com>
2023-12-02 04:52:21 +0300

    * [PopupMenu] Updated descriptions of demo sections

--- demo/src/Views/PopupMenu/PopupMenu.js
+++ demo/src/Views/PopupMenu/PopupMenu.js
@@ -108,7 +108,7 @@ class PopupMenuView extends DemoView {
         this.addSection({
             id: 'toggleOnClick',
             title: '\'toggleOnClick\' option',
-            description: 'With disabled \'toggleOnClick\' option menu will not be opened/closed by click on host element.',
+            description: 'With disabled \'toggleOnClick\' option default handler toggling menu by click will not be added to the host element.',
             content: [
                 createContainer('toggleOnClickContainer', btn.elem),
                 logsField.elem,
@@ -273,6 +273,7 @@ class PopupMenuView extends DemoView {
         this.addSection({
             id: 'list',
             title: 'Attach to list item',
+            description: 'Popup menu should be opened on click by menu button of disabled list item.',
             content: list,
         });
     }
