10b18d552dd4b92f76ce5d3d4b2d9ff50517012e
Ilya Makarov <feesler@gmail.com>
2023-12-10 05:38:29 +0300

    * Updated jezve-test library and added clipboard permissions for Node test environment to config

--- tests/config.js
+++ tests/config.js
@@ -1,4 +1,9 @@
 export const config = {
     nodeURL: 'https://localtest/jezvejs/dist/',
+    nodePermissions: [
+        'clipboard-read',
+        'clipboard-write',
+        'clipboard-sanitized-write',
+    ],
     testsExpected: 1100,
 };
