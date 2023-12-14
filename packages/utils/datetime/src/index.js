2bb65d48e2818781edca2b6f361c02b8d1dd0210
Ilya Makarov <feesler@gmail.com>
2023-12-14 07:50:59 +0300

    + Added getWeek() and getLastDayOfMonth() functions

--- packages/utils/datetime/src/index.js
+++ packages/utils/datetime/src/index.js
@@ -117,10 +117,43 @@ export const shiftDate = (date, shift) => (
 );
 
 /**
+ * Returns the ISO week of the date
+ *
+ * @param {number} timestamp
+ * @returns {number}
+ */
+export const getWeek = (timestamp) => {
+    const date = new Date(timestamp);
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year
+    date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+    // January 4 is always in week 1
+    const week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1
+    const diff = date.getTime() - week1.getTime();
+
+    return 1 + Math.round((diff / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7);
+};
+
+/**
+ * Returns last day in month of the specified date
+ *
+ * @param {Date} date
+ * @returns {Date}
+ */
+export const getLastDayOfMonth = (date) => (
+    new Date(Date.UTC(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0,
+    ))
+);
+
+/**
  * Return count of days in month of the specified date
  */
 export const getDaysInMonth = (date) => {
-    const monthDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    const monthDate = getLastDayOfMonth(date);
     return monthDate.getDate();
 };
 
